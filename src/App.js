48de20afcb92903fa1e06e5dfc2bc0a227c2e82b
shikhar13012001 <75368010+shikhar13012001@users.noreply.github.com>
2021-08-11 14:36:42 +0530

    fixed_shit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import React from 'react';
+const particleOptions={
+  particles: {
+      line_linked:{
+        shadow:{
+          enable: true,
+          color:'#3CA9D1',
+          blur: 5
 
-function App() {
-  return (
+        },
+        distance:150
+      },
+      number: {
+        value:80,
+        density:{
+          enable: true,
+          value_area:800
+        }
+      }
+  }
+  }
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+  input:''
+    }
+  }
+
+  onInputChange=(e)=>{
+  console.log(e.target.value)
+  }
+  handleSubmit=()=>{
+    console.log('click');
+  }
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <Particles  className='particles'
+                params={particleOptions} />
+  <Navigation/>
+  <Logo />
+  <ImageLinkForm onInputChange={this.onInputChange} handleSubmit={this.handleSubmit}/>
+  <Rank/>
+  {/*
+  
+  <faceRecoginition/> */}
     </div>
-  );
+  );}
 }
 
 export default App;
